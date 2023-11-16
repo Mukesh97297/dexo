14217035dfaf2fc94b80d84e3414710d556a4bbf
Mukesh <mkverma97297@gmail.com>
2023-11-16 17:15:50 +0530

    slider

--- app/pages/slider/page.js
+++ app/pages/slider/page.js
@@ -1,30 +1,30 @@
-'use client';
-import React from 'react'
-import Slider from 'react-slick'
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-function SlickCarousel() {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-      };
+// 'use client';
+// import React from 'react'
+// import Slider from 'react-slick'
+// import "slick-carousel/slick/slick.css"; 
+// import "slick-carousel/slick/slick-theme.css";
+// function SlickCarousel() {
+//     const settings = {
+//         dots: true,
+//         infinite: true,
+//         speed: 500,
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//       };
     
-  return (
-    <Slider {...settings}>
-    <div>
+//   return (
+//     <Slider {...settings}>
+//     <div>
         
-    </div>
-    <div>
-      <h3>Slide 2</h3>
-    </div>
-    <div>
-      <h3>Slide 3</h3>
-    </div>
-  </Slider>
-  )
-}
+//     </div>
+//     <div>
+//       <h3>Slide 2</h3>
+//     </div>
+//     <div>
+//       <h3>Slide 3</h3>
+//     </div>
+//   </Slider>
+//   )
+// }
 
-export default SlickCarousel
+// export default SlickCarousel
