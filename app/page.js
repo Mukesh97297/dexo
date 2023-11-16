7c4514cb8d5d6b718707d47f7b265842ddcdd28e
Mukesh <mkverma97297@gmail.com>
2023-11-16 17:19:24 +0530

    slider

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
 
 // import SlickCarousel from "./pages/slider/page";
 
+import Footer from "./pages/footer/page";
+import Navbar from "./pages/header/page";
+
 export default function Home() {
   return (
    <div>
+    <Navbar/>
+    <Footer/>
     {/* <SlickCarousel/> */}
    </div>
   )
