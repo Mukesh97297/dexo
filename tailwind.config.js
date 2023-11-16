f95420b77fb9323a397dee5d52c29d16c74750a2
Mukesh <mkverma97297@gmail.com>
2023-11-16 17:10:18 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-radial':'radial-gradient(58.43% 58.43% at 50% 0%, #0E1330 0%, #060B27 100%)',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'gradient-custom': 'linear-gradient(90deg, #C561FF 0%, #5533FF 100%)',
+          'back-color': "url('/images/Background.png')",
+          'hero-back-img':"url('/images/Background-color.png')",
+          'rectangle-8': "url('/images/Rectangle 8.png')",
       },
     },
   },
